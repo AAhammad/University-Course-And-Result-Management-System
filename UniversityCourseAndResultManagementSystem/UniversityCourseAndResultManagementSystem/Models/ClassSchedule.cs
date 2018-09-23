d8c688ae510502de99746f993b1837f1a003eea4
AAhammad <abeg42096@gmail.com>
2018-09-23 18:10:37 +0600

    adding model

---
+++ UniversityCourseAndResultManagementSystem/UniversityCourseAndResultManagementSystem/Models/ClassSchedule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UniversityCourseAndResultManagementSystem.Models
+{
+    public class ClassSchedule
+    {
+        public int CourseId { get; set; }
+        public int DepartmentId { get; set; }
+        public string CourseCode { get; set; }
+        public string CourseName { get; set; }
+        public string Schedule { get; set; }
+    }
+}
